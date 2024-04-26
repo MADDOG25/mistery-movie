fbc98854e69004a39ae68cc5e2347a54f26ceea5
Jeffrey Morgan <mdog1525@gmail.com>
2024-04-26 00:54:57 -0500

    add components-Header

---
+++ src/components/Movie.tsx
@@ -0,0 +1,7 @@
+// Componente para mostrar información sobre una película individual.
+
+function Movie() {
+  return <div>Movie</div>;
+}
+
+export default Movie;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+//Página de inicio que muestra las películas populares.
+import Header from "../components/Header";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default HomePage;
