971a831ec38845366d5a88f1ebfcc2fa92283f23
Dev Jeffrey <mdog1525@gmail.com>
2024-05-02 18:06:19 -0500

    feature component and daisyui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 //Página de inicio que muestra las películas populares.
+import Feature from "../components/Feature";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -7,6 +8,7 @@ function HomePage() {
     <>
       <Header />
       <Hero />
+      <Feature/>
     </>
   );
 }
